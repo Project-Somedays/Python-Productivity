df8fa8eaeba42b68fb7fdff3ca3c50c79aede148
Project Somedays <projectsomedays@gmail.com>
2023-07-07 18:22:05 +1000

    Now opens the file after creation

--- ProcessingProjectGenerator/main.py
+++ ProcessingProjectGenerator/main.py
@@ -2,9 +2,9 @@
 Creates a copy of the template in a target folder
 """
 
-import PySimpleGUI as sg
 import os
 import shutil
+import PySimpleGUI as sg
 
 TEMPLATE_PATH = r"C:\Users\proje\OneDrive\Documents\Python-Productivity\ProcessingProjectGenerator\template.pde"
 
@@ -12,14 +12,26 @@ TEMPLATE_PATH = r"C:\Users\proje\OneDrive\Documents\Python-Productivity\Processi
 def get_folder_and_filename() -> dict[str, str]:
     layout = [
         [sg.T("Project Name: "), sg.In(key="PROJ")],
-        [sg.FolderBrowse(key="FOLDER")],
+        [
+            sg.T("Choose project root folder"),
+            sg.FolderBrowse(key="FOLDER"),
+            sg.T(key="PATH"),
+        ],
         [sg.Submit(), sg.Cancel()],
     ]
-
     window = sg.Window(title="Set Project name and Folder", layout=layout)
-    event, value = window.read()
+    while True:
+        event, values = window.read()
+        if event in ["Cancel", sg.WIN_CLOSED]:
+            break
+        elif event == "FOLDER":
+            window["PATH"].update(values["FOLDER"])
+        elif event == "Submit":
+            break
+        else:
+            pass
     window.close()
-    return value
+    return values
 
 
 def main():
@@ -33,11 +45,12 @@ def main():
     if os.path.exists(new_path):
         print("That already exists! Belay that override!")
         return
-
     os.mkdir(new_path)
     new_file_path = os.path.join(new_path, project_name + ".pde")
     print(f"Copying the template file into new directory: {new_file_path}")
     shutil.copy(TEMPLATE_PATH, new_file_path)
+    print("Opening the file")
+    os.startfile(new_file_path)
 
 
 if __name__ == "__main__":
